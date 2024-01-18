e7bcc3ce26662126fec28105e4cc25423fe475b5
Danna Bohnhoff <dannab101@gmail.com>
2024-01-17 21:06:23 -0500

    add style_your_own

--- blackjack.ts
+++ blackjack.ts
@@ -1,3 +1,9 @@
+import { shuffle, createDeck, dealCard } from './deck'; 
+// Cannot find module './deck' or its corresponding type declarations.ts(2307)
+
+
+
+
 
 var dealerSum = 0;
 var yourSum = 0;
@@ -5,8 +11,8 @@ var yourSum = 0;
 var dealerAceCount = 0;
 var yourAceCount = 0; 
 
-var hidden;
-var deck;
+var hidden: string;
+var deck: string[];
 
 var canHit = true; 
 
@@ -41,46 +47,30 @@ function shuffleDeck() {
 }
 
 function startGame() {
-    hidden = deck.pop();
-    dealerSum += getValue(hidden);
-    dealerAceCount += checkAce(hidden);
+    let card = deck.pop()!;
+    dealerSum += getValue(card!);
+    dealerAceCount += checkAce(card!);
     
     while (dealerSum < 17) {
        
         let cardImg = document.createElement("img");
         let card = deck.pop();
         cardImg.src = "./cards/" + card + ".png";
-        dealerSum += getValue(card);
-        dealerAceCount += checkAce(card);
-        document.getElementById("dealer-cards").append(cardImg);
+        dealerSum += getValue(card!);
+        dealerAceCount += checkAce(card!);
+        const dealerCardsElement = document.getElementById("dealer-cards");
+        if (dealerCardsElement) {
+            dealerCardsElement.append(cardImg);
+        }
     }
     
-    for (let i = 0; i < 2; i++) {
-        let cardImg = document.createElement("img");
-        let card = deck.pop();
-        cardImg.src = "./cards/" + card + ".png";
-        yourSum += getValue(card);
-        yourAceCount += checkAce(card);
-        document.getElementById("your-cards").append(cardImg);
-    }
-
-   
-    document.getElementById("hit").addEventListener("click", hit);
-    document.getElementById("stay").addEventListener("click", stay);
-    document.getElementById("deal").addEventListener("click", deal);
+    document.getElementById("hit")!.addEventListener("click", hit);
+    document.getElementById("stay")!.addEventListener("click", stay);
+    document.getElementById("deal")!.addEventListener("click", deal);
 }
 
 function hit() {
-    if (!canHit) {
-        return;
-    }
-
-    let cardImg = document.createElement("img");
-    let card = deck.pop();
-    cardImg.src = "./cards/" + card + ".png";
-    yourSum += getValue(card);
-    yourAceCount += checkAce(card);
-    document.getElementById("your-cards").append(cardImg);
+    let card: string = deck.pop()!;
 
     if (reduceAce(yourSum, yourAceCount) > 21) { //A, J, 8 -> 1 + 10 + 8
         canHit = false;
@@ -93,7 +83,7 @@ function stay() {
     yourSum = reduceAce(yourSum, yourAceCount);
 
     canHit = false;
-    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+    (document.getElementById("hidden") as HTMLImageElement).src = "./cards/" + hidden + ".png";
 
     let message = "";
     if (yourSum > 21) {
@@ -127,32 +117,48 @@ function stay() {
         message = "You Lose!";
     }
 
-    document.getElementById("dealer-sum").innerText = dealerSum;
-    document.getElementById("your-sum").innerText = yourSum;
-    document.getElementById("results").innerText = message;
+    const dealerSumElement = document.getElementById("dealer-sum");
+    if (dealerSumElement) {
+        dealerSumElement.innerText = String(dealerSum);
+    }
+
+    const yourSumElement = document.getElementById("your-sum");
+    if (yourSumElement) {
+        yourSumElement.innerText = String(yourSum);
+    }
+
+    const resultsElement = document.getElementById("results");
+    if (resultsElement) {
+        resultsElement.innerText = message;
+    }
 }
 
-function getValue(card) {
+function getValue(card: string) {
     let data = card.split("-"); 
-    let value = data[0];
+    let value = parseInt(data[0]); // Parse the value as an integer
 
     if (isNaN(value)) { 
-        if (value == "A") {
+        if (data[0] == "A") {
             return 11;
         }
         return 10;
+        
+        // if (value === "A") { // Fix: Use triple equals operator (===) instead of double equals operator (==)
+        //     return 11;
+        // }
+        // return 10;
     }
-    return parseInt(value);
+    return value;
 }
 
-function checkAce(card) {
+function checkAce(card: string) {
     if (card[0] == "A") {
         return 1;
     }
     return 0;
 }
 
-function reduceAce(playerSum, playerAceCount) {
+function reduceAce(playerSum: number, playerAceCount: number) {
     while (playerSum > 21 && playerAceCount > 0) {
         playerSum -= 10;
         playerAceCount -= 1;
@@ -162,41 +168,41 @@ function reduceAce(playerSum, playerAceCount) {
 
 
 
-// function canSplit(hand) {
-//     return hand.length === 2 && hand[0].split(" ")[0] === hand[1].split(" ")[0];
-//   }
+function canSplit(hand: string[]) {
+    return hand.length === 2 && hand[0].split(" ")[0] === hand[1].split(" ")[0];
+}
+
+// Function to perform the split
+function splitHand(playerHand: string[], deck: string[]) {
+    if (canSplit(playerHand)) {
+        const cardToMove = playerHand.pop();
+        const newHand = [cardToMove, dealCard(deck)];
+        return [playerHand, newHand];
+    } else {
+        console.log("Split not possible for the current hand.");
+        return [playerHand];
+    }
+}
   
-//   // Function to perform the split
-//   function splitHand(playerHand, deck) {
-//     if (canSplit(playerHand)) {
-//       const cardToMove = playerHand.pop();
-//       const newHand = [cardToMove, dealCard(deck)];
-//       return [playerHand, newHand];
-//     } else {
-//       console.log("Split not possible for the current hand.");
-//       return [playerHand];
-//     }
-//   }
-  
-//   // Example usage
-//   function playBlackjack() {
-//     let deck = createDeck();
-//     shuffle(deck);
-  
-//     // Deal initial hands
-//     let playerHand = [dealCard(deck), dealCard(deck)];
-  
-//     // Check if the player can split
-//     if (canSplit(playerHand)) {
-//       console.log("You can split your hand.");
-//       const [hand1, hand2] = splitHand(playerHand, deck);
-//       console.log("Hand 1:", hand1);
-//       console.log("Hand 2:", hand2);
-//     } else {
-//       console.log("You cannot split your hand.");
-//       console.log("Your hand:", playerHand);
-//     }
-//   }
+  // Example usage
+  function playBlackjack() {
+    let deck = createDeck();
+    shuffle(deck);
+
+    // Deal initial hands
+    let playerHand = [dealCard(deck), dealCard(deck)];
+
+    // Check if the player can split
+    if (canSplit(playerHand)) {
+        console.log("You can split your hand.");
+        const [hand1, hand2] = splitHand(playerHand, deck);
+        console.log("Hand 1:", hand1);
+        console.log("Hand 2:", hand2);
+    } else {
+        console.log("You cannot split your hand.");
+        console.log("Your hand:", playerHand);
+    }
+  }
   
 function deal() {
     location.reload();
